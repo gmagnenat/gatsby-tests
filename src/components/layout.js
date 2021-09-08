a75f53e4f7245ad1df462641cc37430db495d9d1
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-09-08 10:47:15 +0200

    Update styling, update homepage with cta button, prepare mobile navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,10 +4,25 @@ import Seo from '../components/seo';
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <section className='pb-20'>
       <Seo />
-      <nav className='w-full px-9'>
-        <ul className='flex'>
+      <nav className='main-navigation flex justify-between items-center py-8 px-4 xl:px-10'>
+        <Link className='text-lg font-semibold' to='/'>
+          gM
+        </Link>
+        <div className='lg:hidden'>
+          <button className='navbar-burger flex items-center p-3 hover:bg-gray-50 rounded'>
+            <svg
+              className='block h-4 w-4'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <title>Mobile menu</title>
+              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
+            </svg>
+          </button>
+        </div>
+        <ul className='hidden lg:flex lg:ml-auto lg:mr-12 lg:items-center lg:w-auto lg:space-x-12'>
           <li className='mr-2'>
             <Link to='/'>Home</Link>
           </li>
@@ -19,13 +34,13 @@ const Layout = ({ pageTitle, children }) => {
           </li>
         </ul>
       </nav>
-      <main className='w-full px-9'>
-        <h1 className='text-5xl my-8 text-gray-800 font-bold mx-0'>
+      <div className='container px-4 mx-auto pt-12'>
+        <h4 className='mt-8 mb-6 lg:mb-12 font-semibold text-blue-600'>
           {pageTitle}
-        </h1>
+        </h4>
         {children}
-      </main>
-    </div>
+      </div>
+    </section>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle='Home Page'>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <ul className='list'>
-        <li className='list-item'>Tailwindcss added ✅</li>
-        <li className='list-item'>Gatsby plugin image added ✅</li>
-        <li className='list-item'>
-          Optimise the image plugin to load resized image depending on viewport
-          size
-        </li>
-        <li>Add custom font-face</li>
-        <li>Handle FOUC for font loading</li>
-      </ul>
-      <StaticImage
+    <Layout pageTitle='Web / Technologies'>
+      <div className='flex flex-wrap items-center -mx-4'>
+        <div className='w-full md:w-1/2 px-4 mb-6 md:mb-0'>
+          <h2 className='mt-8 mb-6 lg:mb-12 text-4xl lg:text-5xl font-semibold'>
+            Learning 3D and animation on the web
+          </h2>
+          <div className='max-w-lg mb-6 lg:mb-12'>
+            <p className='text-xl text-gray-600'>
+              Learning how to use and create 3D animations with Three.js and
+              implement them in a web page. Learning how to work with Gsap and
+              other animation libraries.
+            </p>
+          </div>
+          <div className='flex flex-wrap'>
+            <Link
+              className='primary-button inline-block px-6 py-4 mb-3 mr-4 text-sm leading-normal bg-blue-600 hover:bg-blue-400 text-white rounded transition duration-200 tracking-widest'
+              to='/blog'
+            >
+              Learning logs
+            </Link>
+          </div>
+          <div className='relative w-full md:w-1/2 px-4'></div>
+        </div>
+      </div>
+      {/* <StaticImage
         alt='Tre Cime, Dolimites, Italia'
         src='../images/dolomites-tre-cime.jpg'
         layout='fullWidth'
-      ></StaticImage>
+      ></StaticImage> */}
     </Layout>
   );
 };
