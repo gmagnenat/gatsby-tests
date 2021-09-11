f2b4bf6822a9595711b0f1f175efec03070c62af
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-09-11 09:53:28 +0200

    Add code highlight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,15 @@
-import './src/styles/global.scss';
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
 
-require('prismjs/themes/prism-solarizedlight.css');
-// gatsby-browser.js
+import CodeBlock from './src/components/CodeBlock';
+import './src/styles/global.scss';
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+require('prismjs/themes/prism-twilight.css');
+
+const component = {
+  pre: CodeBlock,
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={component}>{element}</MDXProvider>;
+};
